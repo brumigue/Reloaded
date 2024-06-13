6b012281226ae69ff72a18994e2bf14a879b78bc
Bruno Miguel Luque <brumigue@cbr15s6.42barcelona.com>
2024-06-14 00:37:04 +0200

    ex13

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,16 +6,18 @@
 /*   By: brumigue <brumigue@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 03:08:44 by brumigue          #+#    #+#             */
-/*   Updated: 2024/06/14 00:25:27 by brumigue         ###   ########.fr       */
+/*   Updated: 2024/06/14 00:35:01 by brumigue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if ((nb < 0) || (nb > 12))
-		return (0);
-	else if (nb == 0)
-		return (1);
-	else
-		return (nb * ft_recursive_factorial(nb - 1) * nb);
+	if (nb >= 0 && nb <= 15)
+	{
+		if (nb == 0 || nb == 1)
+			return (1);
+		else
+			return (nb * ft_recursive_factorial(nb - 1));
+	}
+	return (0);
 }
