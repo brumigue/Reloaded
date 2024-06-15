9d1e166e7a45f1e0a0b2c271cc14af65af4692c8
Bruno Miguel Luque <brumigue@cbr13s5.42barcelona.com>
2024-06-15 06:28:45 +0200

    19

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brumigue <brumigue@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 23:49:37 by brumigue          #+#    #+#             */
+/*   Updated: 2024/06/15 06:21:18 by brumigue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_params(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 1;
+		while (j < argc - i)
+		{
+			if (argv[j][0] > argv[j + 1][0])
+			{
+				ft_swap(&argv[j], &argv[j + 1]);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	ft_sort_params(argc, argv);
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
