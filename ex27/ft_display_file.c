658fc79a0f23b1bcf503329eb5b25babae2e3a1c
Bruno Miguel Luque <brumigue@cbr12s6.42barcelona.com>
2024-06-18 01:15:49 +0200

    b

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -6,61 +6,32 @@
 /*   By: brumigue <brumigue@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 00:26:06 by brumigue          #+#    #+#             */
-/*   Updated: 2024/06/18 01:00:16 by brumigue         ###   ########.fr       */
+/*   Updated: 2024/06/18 01:14:48 by brumigue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-#define BUF_SIZE 1
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-}
-
-int	ft_display_file(char **av)
+void	print_file(char const *name)
 {
 	int		fd;
-	char	buf[BUF_SIZE];
+	char	buf[1];
 
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
+	fd = open(name, O_RDONLY);
+	while (read(fd, buf, sizeof(buf)))
 	{
-		ft_putstr("open() error");
-		return (0);
+		write(1, buf, sizeof(buf));
 	}
-	while (read(fd, buf, BUF_SIZE) > 0)
-		write(1, &buf, 1);
-	if (close(fd) == -1)
-	{
-		ft_putstr("close() error");
-		return (1);
-	}
-	return (0);
+	close(fd);
 }
 
-int	main(int ac, char **av)
+int	main(int argc, char **argv)
 {
-	if (ac > 2)
-		ft_putstr("Too many arguments.\n");
-	else if (ac < 2)
-		ft_putstr("File name missing.\n");
-	else if (ac == 2)
-		ft_display_file(av);
+	if (argc == 2)
+		print_file(argv[1]);
+	else if (argc < 2)
+		write(2, "File name missing.\n", 19);
+	else
+		write(2, "Too many arguments.\n", 20);
 	return (0);
 }
